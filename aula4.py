1fbd923748771b10fcafd39b46f649e6e690574b
Rodolfo Oliveira <orodolfoso.dev@gmail.com>
2024-10-29 20:21:43 -0300

    Tipos de dados

---
+++ aula4.py
@@ -0,0 +1,25 @@
+# Tipos int e float
+# int -> Número inteiro
+# O tipo int representa qualquer número 
+# Positivo ou negativo. int sem sinal é considerado
+# positivo.
+
+# print(11) #int
+# print(-11) #int
+# print(0) #int
+
+
+# Float -> Número com ponto flutuante
+# O tipofloat respresnta qualquer número 
+# Positivo ou negativo com ponto flutuante.
+# Float sem sinal é considerado positivo.
+
+# print(1.1, 10.11) 
+# print(0.0, -1.5) 
+
+# A função type mostra o tipo que o Python 
+# Inferiu ao valor.
+print(type (1.5))
+print(type (15))
+print(type (-15))
+print(type ("Rodolfo"), type(0), type(15), type(2.5))
---
+++ aula5.py
@@ -0,0 +1,8 @@
+# Tipo de dados bool (boolean)
+# Ao questionar algo em um programa 
+# Só existem duas respotas possíveis:
+# Sim(True), Não(False).
+# Exitem vários operadores para "questionar".
+# Dentre elas o == , que é um operador lógico que 
+# questiona se um valor é igual a outro.
+
