4c4d6573c62e4997a7e82c9bc4bd9bbd61d24248
Rodolfo Oliveira <orodolfoso.dev@gmail.com>
2024-10-30 21:04:27 -0300

    Conversão de tipos

--- aula5.py
+++ aula5.py
@@ -6,3 +6,9 @@
 # Dentre elas o == , que é um operador lógico que 
 # questiona se um valor é igual a outro.
 
+print(10 == 10) #Sim => True (Verdadeiro)
+print(10 == 11) #Não => False(Falso)
+print(type(True))
+print(type(False))
+print(type(10 == 10))
+print(type(10 == 11)) 
