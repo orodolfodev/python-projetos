999100b0fc29de8ab686a45f5b063580658d84b3
Rodolfo Oliveira <orodolfoso.dev@gmail.com>
2024-11-11 20:59:37 -0300

    Aula de procedência concluído

---
+++ aula10.py
@@ -0,0 +1,7 @@
+concatenacao = 'Rodolfo' + str(1) + ' ' 'Oliveira'
+print(concatenacao)
+
+a_dez_vezes = 'A' * 10
+tres_vezes_rodolfo = 3 * 'Rodolfo'
+print(a_dez_vezes)
+print(tres_vezes_rodolfo)
---
+++ aula12.py
@@ -0,0 +1,11 @@
+nome = 'Lucas de Oliveira'
+altura = 1.58
+peso = 56
+imc = peso / altura ** 2
+
+print(nome,  'tem', altura, 'de altura',)
+print('pesa', peso, 'quilos e seu imc é:', imc)
+
+# Lucas de Oliveira tem 1.58 de altura
+# pesa 56 quilos e seu IMC é
+# 29.320987654320987
