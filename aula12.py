02b1f681f6158319d6aa5af12d0e30629c331373
Rodolfo Oliveira <orodolfoso.dev@gmail.com>
2024-11-12 11:05:40 -0300

    Utilizando o format

--- aula12.py
+++ aula12.py
@@ -3,8 +3,10 @@ altura = 1.58
 peso = 56
 imc = peso / altura ** 2
 
-print(nome,  'tem', altura, 'de altura',)
+print(nome,  'tem', altura, 'de altura,',)
 print('pesa', peso, 'quilos e seu imc é:', imc)
+
+
 
 # Lucas de Oliveira tem 1.58 de altura
 # pesa 56 quilos e seu IMC é
---
+++ aula13.py
@@ -0,0 +1,19 @@
+nome = 'Lucas de Oliveira'
+altura = 1.58
+peso = 56
+imc = peso / altura ** 2
+
+"f-strings"
+linha_1 = f'{nome} tem  {altura:.2f} de altura'
+linha_2 = f'pesa {peso} quilos e seu imc é:'
+linha_3 = f'{imc:.2f}'
+
+print(linha_1)
+print(linha_2)
+print(linha_3)
+
+
+
+# Lucas de Oliveira tem 1.58 de altura
+# pesa 56 quilos e seu IMC é
+# 29.320987654320987
