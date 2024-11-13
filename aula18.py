a21173206848d9caf2d292db4b38e9a2b7b50413
Rodolfo Oliveira <orodolfoso.dev@gmail.com>
2024-11-13 17:32:14 -0300

    Aula de operadores lógicos

---
+++ aula18.py
@@ -0,0 +1,20 @@
+""""
+
+Operadores de comparção (relacionais)
+OP         Significados         Exemplo True
+>          maior                2 >  1
+>=         maior ou igual       2 >= 1
+<          menor                1 <  2
+<=         menor ou igual       2 <= 2
+==         igual                'a' == 'a'
+!=         diferente            'a' != 'b'
+
+"""
+
+maior = 2 > 1
+maior_ou_igual = 2 >= 2
+menor = 1 < 2
+menor_ou_igual =  2 <= 2
+igual =  'a' == 'a'
+diferente = 'a' != 'b'
+print('Olha meu print aqui!')
---
+++ aula19.py
@@ -0,0 +1,12 @@
+primeiro_valor = input('Digite o primeiro valor: ')
+segundo_valor = input('Digite o segundo valor: ')
+
+if primeiro_valor >= segundo_valor:
+    print(f'{primeiro_valor=} é maior ou igual '
+    f'ao {segundo_valor=}')
+
+else:
+    print(f'{segundo_valor=} é maior '
+    f'do que o {primeiro_valor=}')
+
+print('Fim da execução')
